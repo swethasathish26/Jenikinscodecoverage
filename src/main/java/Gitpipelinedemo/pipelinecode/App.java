c306283e48ea77ac2aaa82c6acab0ab16691b0c7
swetha <swetha.shivashankar@gmail.com>
2020-07-16 11:15:09 +0530

    hello

---
+++ src/main/java/Gitpipelinedemo/pipelinecode/App.java
@@ -0,0 +1,13 @@
+package Gitpipelinedemo.pipelinecode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
